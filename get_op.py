09a893d13ecdad5b0e2d49cb01e9a4ab1b6cd8c9
iortio <98342295+iortio@users.noreply.github.com>
2022-02-07 13:32:22 +0100

    Update get_op.py

--- get_op.py
+++ get_op.py
@@ -1,16 +1,32 @@
 import os
+import sys
 import argparse
+import yaml
+import httpx
 
 
 # Parse arguments
 parser = argparse.ArgumentParser(description='Build functions')
 parser.add_argument('-r', '--repo', default='', nargs='?', help='Repo')
 parser.add_argument('-t', '--token', default='', nargs='?', help='Token')
-parser.add_argument('-p', '--path', default='.', nargs='?', help='Path to store op')
 arguments = parser.parse_args()
 
 
-# Create path
-if arguments.path and arguments.repo and arguments.token:
-    # Get code
-    os.system('git clone https://%s@github.com/%s %s' % (arguments.token, arguments.repo, os.path.join(arguments.path, '.'.join(arguments.repo.split('/')[-1].split('.')[:-1]))))
+# Pull or clone repo
+if arguments.repo and arguments.token:
+    # Define repo auxiliar names
+    full_repo_name = '.'.join(arguments.repo.split('https://github.com/')[1].split('.')[:-1])
+    repo_name = '.'.join(arguments.repo.split('/')[-1].split('.')[:-1])
+
+    # Get configuration
+    url = 'https://%s@raw.githubusercontent.com/%s/main/config.yaml' % (arguments.token, full_repo_name)
+    config = yaml.load(httpx.get(url).text, Loader=yaml.Loader)
+
+    # Install packages
+    if config[0].get('platform', 'python') == 'python':
+        # Python installation
+        install_path = max([elem for elem in sys.path if elem.endswith('dist-packages')], key=len)
+        if os.path.isdir(os.path.join(install_path, repo_name)):
+            os.system('cd %s && git pull origin main' % (os.path.join(install_path, repo_name)))
+        else:
+            os.system('git clone https://%s@github.com/%s %s' % (arguments.token, full_repo_name, os.path.join(install_path, repo_name)))
