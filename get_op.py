d5d848ca285942deb53de68d3533686dc07d5076
iortio <98342295+iortio@users.noreply.github.com>
2022-02-07 13:51:28 +0100

    Update get_op.py

--- get_op.py
+++ get_op.py
@@ -23,10 +23,21 @@ if arguments.repo and arguments.token:
     config = yaml.load(httpx.get(url).text, Loader=yaml.Loader)
 
     # Install packages
-    if config[0].get('platform', 'python') == 'python':
+    if config[0].get('platform', '') == 'python':
         # Python installation
         install_path = max([elem for elem in sys.path if elem.endswith('dist-packages')], key=len)
         if os.path.isdir(os.path.join(install_path, repo_name)):
             os.system('cd %s && git pull origin main' % (os.path.join(install_path, repo_name)))
         else:
             os.system('git clone https://%s@github.com/%s %s' % (arguments.token, full_repo_name, os.path.join(install_path, repo_name)))
+    elif config[0].get('platform', '') == 'make':
+        # Makefile installation
+        install_path = '/opt/%s' % (repo_name)
+        # Get code
+        if os.path.isdir(os.path.join(install_path, repo_name)):
+            os.system('cd %s && git pull origin main' % (install_path))
+        else:
+            os.system('git clone https://%s@github.com/%s %s' % (arguments.token, full_repo_name, install_path))
+        # Execute make
+        os.system('cd %s && make && make install' % (install_path))
+            
