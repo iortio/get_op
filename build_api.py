4e58787c07f724766cda3ea4b377e9880da03a72
iortio <98342295+iortio@users.noreply.github.com>
2022-02-08 15:39:26 +0100

    Create build_api.py

---
+++ build_api.py
@@ -0,0 +1,22 @@
+import os
+
+
+# Create API file
+fo = open('main.py', 'w')
+
+# Add header
+fo.write('from typing import Optional, Union\n')
+fo.write('from fastapi import FastAPI\n')
+fo.write('from pydantic import BaseModel\n\n')
+fo.write('app = FastAPI()\n\n')
+
+# Iterate over endpoints
+for root, dirs, files in os.walk("endpoints", topdown=False):
+    for name in files:
+        if name == 'main.py':
+            fo.write('# %s\n' % (root))
+            fo.write(open(os.path.join(root, name), 'r').read())
+            fo.write('\n\n')
+
+# Close API file
+fo.close()
