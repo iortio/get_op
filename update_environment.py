f6658400ee89f1642041be16ca427892e5abc7b7
iortio <98342295+iortio@users.noreply.github.com>
2022-02-14 09:25:30 +0100

    Update update_environment.py

--- update_environment.py
+++ update_environment.py
@@ -7,7 +7,6 @@ import httpx
 
 # Parse arguments
 parser = argparse.ArgumentParser(description='Build functions')
-parser.add_argument('-e', '--environment', default='', nargs='?', help='Environment repo')
 parser.add_argument('-o', '--op', default='', nargs='?', help='Op repo')
 parser.add_argument('-j', '--job', default='', nargs='?', help='Job repo')
 parser.add_argument('-t', '--token', default='', nargs='?', help='Token')
@@ -19,40 +18,62 @@ envs_path = '/opt/environments'
 if not os.path.isdir(envs_path):
     os.mkdir(envs_path)
 
+repo_path = '/opt/repos'
+if not os.path.isdir(repo_path):
+    os.mkdir(repo_path)
+
+# Get name of repo
+if arguments.op:
+    repo = arguments.op
+elif arguments.job:
+    repo = arguments.job
+else:
+    repo = None
+
 
 # Pull or clone repo
-if arguments.environment and arguments.token and (arguments.op or arguments.job):
+if arguments.token and repo:
     # Define repo auxiliar names
-    full_repo_name = '.'.join(arguments.environment.split('https://github.com/')[1].split('.')[:-1])
-    repo_name = '.'.join(arguments.environment.split('/')[-1].split('.')[:-1])
+    full_repo_name = '.'.join(repo.split('https://github.com/')[1].split('.')[:-1])
+    repo_name = '.'.join(repo.split('/')[-1].split('.')[:-1])
 
-    # Clone into environments folder
-    if os.path.isdir(os.path.join(envs_path, repo_name)):
-        os.system('cd %s/%s && git pull origin main' % (envs_path, repo_name))
+    # Clone into repos folder
+    if os.path.isdir(os.path.join(repo_path, repo_name)):
+        os.system('cd %s/%s && git pull origin main' % (repo_path, repo_name))
     else:
-        os.system('git clone https://%s@github.com/%s %s/%s' % (arguments.token, full_repo_name, envs_path, repo_name))
+        os.system('git clone https://%s@github.com/%s %s/%s' % (arguments.token, full_repo_name, repo_path, repo_name))
 
-    # Get configuration
-    config = yaml.load(open('%s/%s/config.yaml' % (envs_path, repo_name)), Loader=yaml.Loader)
+    # Read configuration
+    config = yaml.load(open('%s/%s/config.yaml' % (repo_path, repo_name)), Loader=yaml.Loader)
 
-    update_environment = False
-    # Add ops
-    if arguments.op:
-        if not config.get('ops', []): config['ops'] = []
-        if not arguments.op in config['ops']:
-            config['ops'].append(arguments.op)
+    # Iterate over environments
+    for environment in config[0].get('environments', []):
+        # Define repo auxiliar names
+        full_env_name = '.'.join(environment.split('https://github.com/')[1].split('.')[:-1])
+        env_name = '.'.join(environment.split('/')[-1].split('.')[:-1])
+        # Clone environment
+        if os.path.isdir(os.path.join(envs_path, env_name)):
+            os.system('cd %s/%s && git pull origin main' % (envs_path, env_name))
+        else:
+            os.system('git clone https://%s@github.com/%s %s/%s' % (arguments.token, full_env_name, envs_path, env_name))
+        # Read env configuration
+        env_config = yaml.load(open('%s/%s/config.yaml' % (envs_path, env_name)), Loader=yaml.Loader)
+        update_environment = False
+        # Add op to config if required
+        if arguments.op and arguments.op not in env_config.get('ops', []):
+            if not 'ops' in env_config: env_config['ops'] = []
+            env_config['ops'].append(arguments.op)
+            update_environment = True
+        # Add job to config if required
+        if arguments.job and arguments.job not in env_config.get('jobs', []):
+            if not 'jobs' in env_config: env_config['jobs'] = []
+            env_config['ops'].append(arguments.job)
             update_environment = True
 
-    # Add jobs
-    if arguments.job:
-        if not config.get('jobs', []): config['jobs'] = []
-        if not arguments.job in config['jobs']:
-            config['jobs'].append(arguments.job)
-            update_environment = True
+        if update_environment:
+            # Save config
+            open('%s/%s/config.yaml' % (envs_path, env_name), 'w').write(yaml.dump(config))
 
-    if update_environment:
-        # Save config
-        open('%s/%s/config.yaml' % (envs_path, repo_name), 'w').write(yaml.dump(config))
+            # Update repo
+            os.system('cd %s/%s && git add . && git commit -m "Environment updated"  && git push origin main' % (envs_path, env_name))
 
-        # Update repo
-        os.system('cd %s/%s && git add . && git commit -m "Environment updated"  && git push origin main' % (envs_path, repo_name))
